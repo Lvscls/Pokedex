196ec6b62f5fba3a1171ad09be8306bfeb153041
Lvscls <lilianvscls@gmail.com>
2022-04-15 20:15:34 +0200

    <commit>

---
+++ src/routes/about.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>About the Svelte Kit Pokedex</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>this is a really cool Svelte kit demo that uses the pokemon API to build pokedex</p>
